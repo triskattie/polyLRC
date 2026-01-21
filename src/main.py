d017fa2d8a78e41595e2a46a11935b1f005e0f7a
triskattie <tristankoetsier01@outlook.com>
2026-01-21 12:40:32 +0100

    Tiny fixes

--- src/main.py
+++ src/main.py
@@ -5,10 +5,3 @@ from src.api.routers import health
 app = FastAPI()
 
 app.include_router(health.router)
-
-
-def main():
-    uvicorn.run("main:app", host="0.0.0.0")
-
-if __name__ == "__main__":
-    main()
