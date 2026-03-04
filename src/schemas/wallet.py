054df38bfff886775d52871cd526a2f368e11318
triskattie <tristankoetsier01@outlook.com>
2026-03-04 09:06:42 +0100

    Fixed wrong datatypes

--- src/schemas/wallet.py
+++ src/schemas/wallet.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
-from uuid import UUID
 from decimal import Decimal
 
 class WalletResponse(BaseModel):
-    user_id: UUID
+    user_id: str
     balance: Decimal
--- src/services/wallets.py
+++ src/services/wallets.py
@@ -4,7 +4,7 @@ from src.core.errors import WalletNotFound
 from src.schemas.wallet import WalletResponse
 
 async def wallet_service(user_id: str, db: AsyncSession):
-    wallet = get_wallet_by_user(user_id=user_id, db=db)
+    wallet = get_wallet_by_user(user_id=UUID(user_id), db=db)
     if not wallet:
         raise WalletNotFound()
     balance = get_balance(wallet_id=wallet.id, db=db)
