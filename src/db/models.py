7d8339a43b23a78892e59f2f3398fa18b5bb9415
triskattie <tristankoetsier01@outlook.com>
2026-03-04 01:37:27 +0100

    Added Wallet and WalletTransaction models

--- src/db/models.py
+++ src/db/models.py
@@ -1,7 +1,14 @@
-from sqlalchemy import String, Integer, Column, Boolean, UUID, DateTime, ForeignKey
+from sqlalchemy import String, Integer, Column, Boolean, UUID, DateTime, ForeignKey, Numeric
 from src.db.base import Base
 from sqlalchemy.sql import func
 from uuid import uuid4
+from enum import Enum
+
+class TransactionType(Enum):
+    FAUCET = "FAUCET"
+    TRADE = "TRADE"
+    ADMIN_ADJUST = "ADMIN_ADJUST"
+
 
 class User(Base):
     __tablename__ = "users"
@@ -21,5 +28,25 @@ class RefreshToken(Base):
     token_hash = Column(String, nullable=False)
     expires_at = Column(DateTime, nullable=False)
     revoked = Column(Boolean, default=False)
+
+    created_at = Column(DateTime, server_default=func.now())
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    created_at = Column(DateTime, server_default=func.now())
+
+class WalletTransaction(Base):
+    __tablename__ = "wallet_transactions"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    wallet_id = Column(UUID(as_uuid=True), ForeignKey("wallets.id", ondelete="CASCADE"), nullable=False)
+
+    amount = Column(Numeric(precision=18, scale=8), nullable=False)
+    type = Column(Enum(TransactionType, native_enum=False), nullable=False)
+    related_market_id = Column(UUID(as_uuid=True), nullable=True) # For adding foreign key to markets later
 
     created_at = Column(DateTime, server_default=func.now())
