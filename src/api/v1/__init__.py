c3c5156c8824740b679eb28177a0c1d69cfb5b2f
triskattie <tristankoetsier01@outlook.com>
2026-03-05 18:36:56 +0100

    Initial market router creation

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -2,6 +2,7 @@ from .routers.auth import router as auth_router
 from .routers.health import router as health_router
 from .routers.users import router as user_router
 from .routers.wallets import router as wallet_router
+from .routers.markets import router as market_router
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/v1")
@@ -10,3 +11,4 @@ router.include_router(auth_router)
 router.include_router(health_router)
 router.include_router(user_router)
 router.include_router(wallet_router)
+router.include_router(market_router)
