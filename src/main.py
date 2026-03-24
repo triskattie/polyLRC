27c8d90f4442da80df81fd1b8e17eece1c0c6677
triskattie <tristankoetsier01@outlook.com>
2026-03-24 01:30:36 +0100

    Preparations for production

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ app.include_router(v1router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://192.168.2.36:3000"],
+    allow_origins=["http://localhost:3000", "http://192.168.2.36:3000", "https://polylrc.triskattie.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
